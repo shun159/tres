49d40bf2c011ac638562c7322b57144485c33a17
Eishun Kondoh <shun3382@gmail.com>
2017-12-01 11:33:53 +0900

    Fix to work on Aruba

--- examples/heckle/config/config.exs
+++ examples/heckle/config/config.exs
@@ -5,23 +5,23 @@ use Mix.Config
 config :heckle,
   vlan_tagging:  true,
   vlan_id:       123,
-  vlan_trunk:    "veth0",
-  access_port1:  "veth4",
-  access_port2:  "veth3",
+  vlan_trunk:    "1",
+  access_port1:  "3",
+  access_port2:  "4",
   receiver_mac:  "00000000000f",
   receiver_ip:   {8,8,8,8},
   sender_mac:    "001122334455",
   inside_local:  {192,168,5,10},
   outside_local: {192,168,255,1},
-  flow_pattern:  :bum
+  flow_pattern:  :bum # :nat || :bum
 
 config :tres,
   protocol:        :tcp,
-  port:            6653,
+  port:            6633,
   max_connections: 10,
   num_acceptors:   10,
   callback_module: Heckle.Controller,
-  callback_args:   ["0000d2851d52d749"]
+  callback_args:   ["0002b05ada98d790"]
 
 config :logger,
   level: :debug,
