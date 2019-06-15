70aac5da905fbd622bbfd102b2e1de2fe1090fb9
Eishun Kondoh <shun3382@gmail.com>
2019-06-15 11:01:51 +0900

    Version bump

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Tres.Mixfile do
   def project do
     [
       app: :tres,
-      version: "0.1.0",
+      version: "0.1.1",
       description: @description,
       elixir: "~> 1.8",
       package: package(),
